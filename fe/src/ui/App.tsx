436e00183ec5c6d385cb67413407c619354bb973
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-09-19 21:59:23 +0300

    frontend init

---
+++ fe/src/ui/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red grid place-items-center h-screen">
+        Hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
