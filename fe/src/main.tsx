5dc836b6caa4d6e46fd1b83153441b8eb151e760
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-09-21 20:34:08 +0300

    SPA github fix

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename="EventHub">
       <ToastContainer />
       <App />
     </BrowserRouter>
